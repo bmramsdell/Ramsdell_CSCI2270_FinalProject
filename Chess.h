2e238c32b6b84b616deffa11c85791755301c4fa
bmramsdell <blaine.ramsdell@colorado.edu>
2015-04-28 16:41:25 -0600

    This is the header file for the simple chess program.

--- Chess.h
+++ Chess.h
@@ -1,12 +1,6 @@
-// Zach Withrow and Blaine Ramsdell
-// April 26, 2015
-// Final Project: Phase 2
-
 #ifndef CHESS_H
 #define CHESS_H
 #include <iostream>
-
-
 struct boardSpace
 {
     std::string name;
@@ -21,6 +15,8 @@ struct boardSpace
     boardSpace *upRight;
     boardSpace *downLeft;
     boardSpace *downRight;
+
+
 };
 
 class Chess
@@ -30,18 +26,18 @@ class Chess
         virtual ~Chess();
         void createBoard(std::string, std::string);
         boardSpace* newSpace(std::string);
-        void team1Board();
-        void team2Board();
-        void PlayerTurn(std::string, std::string);
+        void team1Board(std::string, std::string);
+        void team2Board(std::string, std::string);
+        bool Rook(std::string, boardSpace*, int);
+        bool Knight(std::string, boardSpace*, int);
+        bool Queen(std::string, boardSpace*, int);
+        bool Pawn(std::string, boardSpace*, int);
+        bool King(std::string, boardSpace*, int);
+        bool Bishop(std::string, boardSpace*, int);
         boardSpace* SearchSpot(std::string);
+        void PlayerTurn(std::string, std::string);
         bool checkForKing(std::string);
-        bool Rook(std::string, boardSpace *, int);
-        bool Bishop(std::string, boardSpace *, int);
-        bool Queen(std::string, boardSpace *, int);
-        bool Pawn(std::string, boardSpace *, int);
-        bool King(std::string, boardSpace *, int);
-        bool Knight(std::string, boardSpace *, int);
-        void PawnSwitchOut(boardSpace *,std::string, int);
+        void PawnSwitchOut(boardSpace*,std::string, int);
     protected:
     private:
         boardSpace* beginning;
